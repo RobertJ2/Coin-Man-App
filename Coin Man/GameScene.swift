cb60fd9f227a150305f920aaae63ec42242a6ad5
Robert Jackson Jr <jacksor@bgsu.edu>
2018-06-11 21:47:10 -0400

    Track high score and reset once player crash into enemy object.

--- Coin Man/GameScene.swift
+++ Coin Man/GameScene.swift
@@ -13,9 +13,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var coinMan : SKSpriteNode?
     var coinTimer : Timer?
+    var bombTimer : Timer?
     var ground : SKSpriteNode?
     var ceil : SKSpriteNode?
     var scoreLabel : SKLabelNode?
+    var yourScoreLabel : SKLabelNode?
+    var finalScoreLabel : SKLabelNode?
     
     let coinManCategory : UInt32 = 0x1 << 1
     let coinCategory : UInt32 = 0x1 << 2
@@ -44,17 +47,48 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scoreLabel = childNode(withName: "scoreLabel") as? SKLabelNode
         
         
+        startTimers()
+        
+    }
+    
+    func startTimers() {
+        
         coinTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
             self.createCoin()
         })
+        
+        bombTimer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: { (timer) in
+            self.createBomb()
+        })
+        
     }
     
     
   
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        coinMan?.physicsBody?.applyForce(CGVector(dx: 0, dy: 100000))
         
+        
+        if scene?.isPaused == false {
+            coinMan?.physicsBody?.applyForce(CGVector(dx: 0, dy: 100000))
+        }
+        
+        let touch = touches.first
+        if let location = touch?.location(in: self) {
+            let theNodes = nodes(at: location)
+            
+            for node in theNodes {
+                if node.name == "play" {
+                    score = 0
+                    node.removeFromParent()
+                    finalScoreLabel?.removeFromParent()
+                    yourScoreLabel?.removeFromParent()
+                    scene?.isPaused = false
+                    scoreLabel?.text = "Score: \(score)"
+                    startTimers()
+                }
+            }
+        }
     }
     
   
@@ -84,17 +118,90 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         coin.run(SKAction.sequence([moveLeft,  SKAction.removeFromParent()]))
     }
     
+    func createBomb() {
+        let bomb = SKSpriteNode(imageNamed: "bomb")
+        bomb.physicsBody = SKPhysicsBody(rectangleOf: bomb.size)
+        bomb.physicsBody?.affectedByGravity = false
+        bomb.physicsBody?.categoryBitMask = bombCategory
+        bomb.physicsBody?.contactTestBitMask = coinManCategory
+        bomb.physicsBody?.collisionBitMask = 0
+        addChild(bomb)
+        
+        
+        
+        let maxY = size.height / 2 - bomb.size.height / 2
+        let minY = -size.height / 2 + bomb.size.height / 2
+        let range = maxY - minY
+        let bombY = maxY - CGFloat(arc4random_uniform(UInt32(range)))
+        
+        
+        
+        bomb.position = CGPoint(x: size.width / 2 + bomb.size.width / 2, y: bombY)
+        
+        let moveLeft = SKAction.moveBy(x: -size.width - bomb.size.width, y: 0, duration: 4)
+        
+        
+        bomb.run(SKAction.sequence([moveLeft,  SKAction.removeFromParent()]))
+    }
+    
+    
     func didBegin(_ contact: SKPhysicsContact) {
         
-        score += 1
-        scoreLabel?.text = "Score: \(score)"
+     
         
         if contact.bodyA.categoryBitMask == coinCategory {
             contact.bodyA.node?.removeFromParent()
+            score += 1
+            scoreLabel?.text = "Score: \(score)"
         }
         if contact.bodyB.categoryBitMask == coinCategory {
             contact.bodyB.node?.removeFromParent()
+            score += 1
+            scoreLabel?.text = "Score: \(score)"
         }
+        
+        if contact.bodyA.categoryBitMask == bombCategory {
+            contact.bodyA.node?.removeFromParent()
+            gameOver()
+        }
+        if contact.bodyB.categoryBitMask == bombCategory {
+            contact.bodyB.node?.removeFromParent()
+            gameOver()
+        }
+        
     }
 
+    func gameOver() {
+        scene?.isPaused = true
+        
+        coinTimer?.invalidate()
+        bombTimer?.invalidate()
+        
+        yourScoreLabel = SKLabelNode(text: "Your Score:")
+        yourScoreLabel?.position = CGPoint(x: 0, y: 200)
+        yourScoreLabel?.fontSize = 100
+        yourScoreLabel?.zPosition = 1
+
+        if yourScoreLabel != nil {
+            addChild(yourScoreLabel!)
+        }
+        
+        finalScoreLabel = SKLabelNode(text: "\(score)")
+        finalScoreLabel?.position = CGPoint(x: 0, y: 0)
+        finalScoreLabel?.fontSize = 200
+        finalScoreLabel?.zPosition = 1
+
+        if finalScoreLabel != nil {
+            addChild(finalScoreLabel!)
+        }
+        let playButton = SKSpriteNode(imageNamed: "play")
+        playButton.position = CGPoint(x: 0, y: -200)
+        playButton.name = "play"
+        playButton.zPosition = 1
+        addChild(playButton)
+        
+    }
+    
+    
+    
 }
